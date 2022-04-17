245eaeee09b5e1a0c9f79a21c8db1abc58726a10
antegorov <antegorov@mail.ru>
2022-04-17 12:02:15 +0500

    added products

--- gulpfile.js
+++ gulpfile.js
@@ -75,6 +75,7 @@ function scripts() {
             'node_modules/slick-carousel/slick/slick.js',
             'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
             'node_modules/jquery/dist/jquery.js',
+            'node_modules/mixitup/dist/mixitup.js',
         ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
