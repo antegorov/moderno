c4f677f19f433707b782719f2b1350b8deb97cbe
antegorov <antegorov@mail.ru>
2022-05-07 14:12:00 +0500

    adaptive header and footer, page.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    var mixer = mixitup('.products__inner-box');
+  var mixer = mixitup('.products__inner-box');
 
     $('.product-slider__inner').slick({
         slidesToShow: 4,
@@ -16,7 +16,17 @@ $(function() {
     });
 
 
+    
+    $('.menu__btn').on('click', function () {
+      $('.menu__list').slideToggle(); 
+    });
 
 
+    $('.header__btn-menu').on('click',function () {
+      $('.header__box').toggleClass('active');
+    });
+  
+
+    
 
 });
